391c2872ed3a1b9edb2778e9b3b69c86b47ce21e
Your Name <you@example.com>
2017-02-24 10:44:21 +0100

    V 2.0 index etc.

--- dohled.sh
+++ dohled.sh
@@ -4,18 +4,12 @@ cas=`date +%T" "%D`
 ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
-un=`who | wc -l `
-du=`df -h | grep sda | awk -F " " '{print $3}'`
-da=`df -h | grep sda | awk -F " " '{printf $4}'`
-um=`free -m | grep Mem: | awk -F " " '{print $3}'`
-fm=`free -m | grep Mem: | awk -F " " '{print $4}'`
-echo "Aktuální čas: "$cas >> dohled
-echo "Moje IP: "$ip >> dohled
-echo "Přijatá data: "$rx >> dohled
-echo "Odeslaná data: "$tx >> dohled
+users=`who | wc -l `
 
-echo "Počet přihlášených uživatelů: "$un >> dohled
-echo "Využité místo na disku: "$du >> dohled
-echo "Volné místo na disku: "$da >> dohled
-echo "Využitá RAM: "$um" MB" >> dohled
-echo "volná RAM: "$fm" MB" >> dohled
+echo "Aktuální čas: "$cas >> /home/student/Plocha/skripty/index.html
+echo "Moje IP: "$ip >> /home/student/Plocha/skripty/index.html
+echo "Přijatá data: "$rx >> /home/student/Plocha/skripty/index.html
+echo "Odeslaná data: "$tx >> /home/student/Plocha/skripty/index.html
+echo "Počet přihlášených uživatelů: "$users >> /home/student/Plocha/skripty/index.html
+echo "<br><br>" >> /home/student/Plocha/skripty/index.html
+
