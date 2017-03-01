afbcf93f0b13fe9e5194a116125f12e563757616
Your Name <you@example.com>
2017-03-01 21:43:44 +0100

    V 3.0 dohled.sh, status dodelavka

--- dohled.sh
+++ dohled.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-
-cas=`date +%T" "%D`
 ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
 rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
 tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
-users=`who | wc -l `
+disk=`df -h | grep /dev/sda1 | awk -F " " '{print $4}'`
+users=`who | wc -l`
+cas=`date +"%d.%m.%Y - %H:%M:%S"`
 
-echo "Aktuální čas: "$cas >> /home/student/Plocha/skripty/index.html
-echo "Moje IP: "$ip >> /home/student/Plocha/skripty/index.html
-echo "Přijatá data: "$rx >> /home/student/Plocha/skripty/index.html
-echo "Odeslaná data: "$tx >> /home/student/Plocha/skripty/index.html
-echo "Počet přihlášených uživatelů: "$users >> /home/student/Plocha/skripty/index.html
-echo "<br><br>" >> /home/student/Plocha/skripty/index.html
+echo '<div style="border-bottom: 1px blue;"><h1 style="color: blue; text-align: left;">Moje IP: '$ip'</h1>' >> /home/viktor/Desktop/skripty/index.html
+echo "<p>Přijato: "$rx"</p>" >> /home/viktor/Desktop/skripty/index.html
+echo "<p> Odeslaná data: "$tx"</p>" >> /home/viktor/Desktop/skripty/index.html
+echo "<p>Volné místo na disku: "$disk"B</p>" >> /home/viktor/Desktop/skripty/index.html
+echo "<p>Počet přihlášených uživatelů: "$users "uživatelů</p>" >> /home/viktor/Desktop/skripty/index.html
+echo "<code>Datum: "$cas"</code>" >> /home/viktor/Desktop/skripty/index.html
+echo '<br><br></div>' >> /home/viktor/Desktop/skripty/index.html
+vloz=`cat /home/viktor/Desktop/skripty/index.html
+echo '<html lang="cs"><head>' > /var/www/index.html
+echo '<meta charset="utf-8">' >> /var/www/index.html
+echo '<title>Status</title></head><body>' >> /var/www/index.html
+echo '<div style="font-family: arial, sans-serif; padding: 5px; max-width: 750px; margin: auto; text-align: center; border: 2px solid blue; border-radius: 10px; color: black; background: white">' >> /var/www/index.html
+echo $vloz >> /var/www/index.html
+echo '</div></body></html>' >> /var/www/index.htmlvlozecho
+
+
 
--- ip.sh
+++ ip.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-ip=`ifconfig | grep "inet adr" | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
-echo "Moje IP: "$ip
+ip=`ifconfig | grep Všesměr | awk -F ":" '{print $2}' | awk -F " " '{print $1}'`
+rx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
+tx=`ifconfig eth0 | grep Přijato | awk -F "(" '{print $3}' | awk -F ")" '{print $1}'`
+disk=`df -h | grep /dev/sda1 | awk -F " " '{print $4}'`
+users=`who | wc -l`
+cas=`date +"%d.%m.%Y - %H:%M:%S"`
+echo '<div style="border-bottom: 1px blue;"><h1 style="color: blue; text-align: left;">Moje IP: '$ip'</h1>' >> /home/student/Plocha/skripty/status
+echo "<p>Přijato: "$rx"</p>" >> /home/student/Plocha/skripty/status
+echo "<p> Odeslaná data: "$tx"</p>" >> /home/student/Plocha/skripty/status
+echo "<p>Volné místo na disku: "$disk"B</p>" >> /home/student/Plocha/skripty/status
+echo "<p>Počet přihlášených uživatelů: "$users "uživatelů</p>" >> /home/student/Plocha/skripty/status
+echo "<code>Datum: "$cas"</code>" >> /home/student/Plocha/skripty/status
+echo '<br><br></div>' >> /home/student/Plocha/skripty/status
+vloz=`cat /home/student/Plocha/skripty/status`
+echo '<html lang="cs"><head>' > /var/www/index.html
+echo '<meta charset="utf-8">' >> /var/www/index.html
+echo '<title>Status</title></head><body>' >> /var/www/index.html
+echo '<div style="font-family: arial, sans-serif; padding: 5px; max-width: 750px; margin: auto; text-align: center; border: 2px solid blue; border-radius: 10px; color: black; background: white">' >> /var/www/index.html
+echo $vloz >> /var/www/index.html
+echo '</div></body></html>' >> /var/www/index.html
+
+
 
